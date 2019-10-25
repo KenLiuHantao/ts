7fc477300c921d0e4a3faeaf59757e415633191b
liuhantao <1045543977@qq.com>
2019-10-25 14:53:56 +0800

    test

--- src/page/Hello.tsx
+++ src/page/Hello.tsx
@@ -1,7 +1,6 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react'
 
-class Hello extends Component {
+class Hello extends React.Component {
     render() {
         return (
             <div>
