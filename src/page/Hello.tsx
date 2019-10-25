1082ebf9a2c98586ed564673a00219784858908c
liuhantao <1045543977@qq.com>
2019-10-25 14:35:19 +0800

    test

---
+++ src/page/Hello.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class Hello extends Component {
+    render() {
+        return (
+            <div>
+                <p>Hello world!</p>
+            </div>
+        )
+    }
+
+}
+export default Hello
