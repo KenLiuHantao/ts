aee0afa4e0ad7a03a16da8e8e6eb4dcb73ff2e7c
liuhantao <1045543977@qq.com>
2019-10-25 14:56:45 +0800

    test3

--- src/page/Hello.tsx
+++ src/page/Hello.tsx
@@ -4,7 +4,7 @@ class Hello extends React.Component {
     render() {
         return (
             <div>
-                <p>Hello world2!</p>
+                <p>Hello world3!</p>
             </div>
         )
     }
